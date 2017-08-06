14cb6ae5daa1b947295bab9f4110e1e08a799664
James Lave <james@jameslave.com>
2017-08-06 02:44:49 -0500

    Enable CORS to prevent errors from localhost

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,32 @@
 const functions = require('firebase-functions')
 const nodemailer = require('nodemailer')
+const cors = require('cors')({ origin: true })
 require('dotenv').config()
 
-exports.sendMail = functions.https.onRequest((req, res) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.USER,
-      pass: process.env.PASS
-    }
-  })
-
-  const mailOptions = {
-    from: `"Contact Page" <${process.env.USER}>`,
-    to: process.env.TO,
-    subject: `Message from ${req.body.name}`,
-    html: `<p>${req.body.message}</p><p>Reply to <a href="mailto:${req.body.email}">${req.body.email}</a></p>`
+const transporter = nodemailer.createTransport({
+  host: process.env.HOST,
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS
   }
+})
 
-  transporter.sendMail(mailOptions)
-    .then(res.send('Mail sent'))
-    .catch(err => {
-      console.log(err)
-      res.status(500).send('Mail could not be sent')
-    })
+exports.sendMail = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    const mailOptions = {
+      from: `"Contact Page" <${process.env.USER}>`,
+      to: process.env.TO,
+      subject: `Message from ${req.body.name}`,
+      html: `<p>${req.body.message}</p><p>Reply to <a href="mailto:${req.body.email}">${req.body.email}</a></p>`
+    }
+
+    transporter.sendMail(mailOptions)
+      .then(res.send('Mail sent'))
+      .catch(err => {
+        console.log(err)
+        res.status(500).send('Mail could not be sent')
+      })
+  })
 })
